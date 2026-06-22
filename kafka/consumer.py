1723db3aef9c4e3cf170303b4f27f023d07aa377
SRISHHA <89242466+SRISHHA@users.noreply.github.com>
2026-06-22 12:27:34 +0000

    add

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,1 +1,23 @@
+from kafka import KafkaConsumer
+import json
 
+consumer = KafkaConsumer(
+    "network_logs",
+
+    bootstrap_servers=
+    "kafka-network2026-srishha2001-bd1.i.aivencloud.com:25963",
+
+    security_protocol="SSL",
+
+    ssl_cafile="certs/ca.pem",
+    ssl_certfile="certs/service.cert",
+    ssl_keyfile="certs/service.key",
+
+    value_deserializer=lambda m:
+    json.loads(m.decode("utf-8"))
+)
+
+print("Waiting...")
+
+for msg in consumer:
+    print(msg.value)
--- kafka/producer.py
+++ kafka/producer.py
@@ -1,1 +1,42 @@
+import pandas as pd
+import json
+import time
 
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers="kafka-network2026-srishha2001-bd1.i.aivencloud.com:25963",
+    security_protocol="SSL",
+    ssl_cafile="certs/ca.pem",
+    ssl_certfile="certs/service.cert",
+    ssl_keyfile="certs/service.key",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+df = pd.read_csv("data/UNSW_NB15_training-set.csv")
+
+# Remove target columns
+df = df.drop(
+    columns=["label", "attack_cat"],
+    errors="ignore"
+)
+
+# Testing only
+df = df.head(50)
+
+for index, row in df.iterrows():
+
+    future = producer.send(
+        "network_logs",
+        row.to_dict()
+    )
+
+    future.get(timeout=10)
+
+    print(f"Sent record {index+1}")
+
+    time.sleep(1)
+
+producer.flush()
+
+print("All records sent successfully")
