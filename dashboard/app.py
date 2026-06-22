6ec53cf2e32bff0aa9bb1cdd87169ddea125e8c0
SRISHHA <89242466+SRISHHA@users.noreply.github.com>
2026-06-22 12:48:14 +0000

    add

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,84 @@
+import pandas as pd
+import gradio as gr
+import os
 
+CSV_FILE = "logs/predictions.csv"
+
+
+def load_data():
+
+    if not os.path.exists(CSV_FILE):
+
+        return (
+            0,
+            0,
+            pd.DataFrame(),
+            pd.DataFrame()
+        )
+
+    df = pd.read_csv(CSV_FILE)
+
+    total_packets = len(df)
+
+    total_attacks = len(
+        df[df["prediction"] == "Attack"]
+    )
+
+    attack_counts = (
+        df["attack_type"]
+        .value_counts()
+        .reset_index()
+    )
+
+    attack_counts.columns = [
+        "Attack Type",
+        "Count"
+    ]
+
+    recent = df.tail(20)
+
+    return (
+        total_packets,
+        total_attacks,
+        attack_counts,
+        recent
+    )
+
+
+with gr.Blocks() as demo:
+
+    gr.Markdown(
+        "# 🚨 Real-Time Intrusion Detection Dashboard"
+    )
+
+    total_packets = gr.Number(
+        label="Total Packets"
+    )
+
+    total_attacks = gr.Number(
+        label="Total Attacks"
+    )
+
+    attack_table = gr.Dataframe(
+        label="Attack Distribution"
+    )
+
+    recent_table = gr.Dataframe(
+        label="Recent Predictions"
+    )
+
+    demo.load(
+        load_data,
+        outputs=[
+            total_packets,
+            total_attacks,
+            attack_table,
+            recent_table
+        ],
+        every=2
+    )
+
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=7860
+)
